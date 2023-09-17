4305940c01d52a6019089d3f3869d4f5f237e72e
EgorPoprotskiy <EgorPoprotskiy@gmail.com>
2023-09-17 15:47:06 +1000

    add fragments for notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("kotlin-android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-kapt")
+
 }
 
 android {
@@ -10,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.egorpoprotskiy.rcdm"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -36,19 +41,48 @@ android {
     buildFeatures {
         viewBinding = true
     }
+//    kotlin {
+//        jvmToolchain(17)
+//    }
 }
 
 dependencies {
+    val room_version = "2.5.2"
+    val nav_version = "2.7.2"
+    val cor_version = "1.6.4"
+    val lifecycle_version = "2.6.2"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+    //для фрагментов
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+
+    //Корутины
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$cor_version")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$cor_version")
+
+    // Room libraries
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    // дополнительно - Поддержка расширений Kotlin и сопрограмм для Room
+    implementation("androidx.room:room-ktx:$room_version")
+    // Для использования обработки символов Kotlin (KSP)
+//    ksp("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
 }
---
+++ app/src/main/java/com/egorpoprotskiy/rcdm/NoteDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.egorpoprotskiy.rcdm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NoteDetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_detail, container, false)
+    }
+}
